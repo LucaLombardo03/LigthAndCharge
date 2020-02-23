4b29db141da817f34cc4f83ae9bd58cad23e4240
luca lombardo <lucalombardo03@gmail.com>
2020-02-23 23:23:33 +0100

    Add project files.

---
+++ LigthAndCharge/LigthAndCharge/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace LigthAndCharge
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            InitSwitch();
+        }
+        
+        void InitSwitch()
+        {
+            SwitchT.Toggled += SwitchT_Toggled;
+        }
+        
+        private void SwitchT_Toggled(object sender, ToggledEventArgs e)
+        {
+            DisplayAlert("Lampada", "Luce accesa: " + e.Value, "Ok");
+        }
+    }
+}
